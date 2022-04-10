10132214b527e9d3955eae534598790c8a2c7e5e
feritarou <schwarz.f@web.de>
2022-04-10 12:15:52 -0400

    fix regression type issues

--- src/hasura-client.cr
+++ src/hasura-client.cr
@@ -44,24 +44,25 @@ module Hasura
   macro query(gql_name, **variables)
     {% filename = __DIR__.gsub(/(\/lib\/hasura-client)?\/src\/?$/, "/graphql") + "/" + gql_name.id.stringify + ".gql" %}
     {% raise "GraphQL file '#{gql_name.id}.gql' not found in graphql folder (#{filename.id})" unless `[ -e "#{filename}" ]; echo -n $?` == "0" %}
-
-    response = Hasura.post_request({
-      query: {{ read_file filename }},
-      operationName: {{ gql_name.id.stringify }},
-      variables: { {{ **variables }} } {% if variables.empty? %}of String => String{% end %}
-    }.to_json)
-    r = Hasura::Schema::{{ gql_name.id }}Response.from_json response.body
-    if errs = r.errors
-      err = errs.first
-      raise Hasura::RequestError.new({{gql_name.id.stringify}}, err.message, err.extensions.code, err.extensions.path)
+    begin
+      %response = Hasura.post_request({
+        query: {{ read_file filename }},
+        operationName: {{ gql_name.id.stringify }},
+        variables: { {{ **variables }} } {% if variables.empty? %}of String => String{% end %}
+      }.to_json)
+      %r = Hasura::Schema::{{ gql_name.id }}Response.from_json %response.body
+      if %errs = %r.errors
+        %err = %errs.first
+        raise Hasura::RequestError.new({{gql_name.id.stringify}}, %err.message, %err.extensions.code, %err.extensions.path)
+      end
+      %r.data.not_nil!
     end
-    r.data.not_nil!
   end
 
   # --------------------------------------------------------------------------
 
   macro mutate(gql_name, **variables)
-    Hasura.query {{ gql_name }}{% if !variables.empty? %}, {{ **variables }}{% end %}
+    Hasura.query({{ gql_name }}{% if !variables.empty? %}, {{ **variables }}{% end %})
   end
 
   # ==========================================================================
@@ -69,11 +70,11 @@ module Hasura
   # ==========================================================================
 
   def post_request(request)
-    client.post endpoint, HTTP::Headers{
+    client.post(endpoint, HTTP::Headers{
       "Accept" => "application/json",
       "Accept-Encoding" => "identity",
       "X-Hasura-Admin-Secret" => secret
-    }, request
+    }, request)
   end
 
   # --------------------------------------------------------------------------
